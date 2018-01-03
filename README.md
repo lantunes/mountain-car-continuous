5b0af00f0cea3a9e48e4a00504b55d549e562be5
Luis Antunes <luis.antunes@thomsonreuters.com>
2018-01-03 14:16:06 -0500

    removing unused agents

--- README.md
+++ README.md
@@ -7,16 +7,19 @@ OpenAI Gym (MountainCarContinuous-v0). The code in this repo makes use of the Te
 
 The following algorithms are implemented:
  
-Stochastic Policy Gradient with REINFORCE:
+REINFORCE with Stochastic Policy Gradient:
 - located in the rl.reinforce module
 - uses a Gaussian Policy Gradient
-- mu and sigma are learned separately (i.e. sigma does not depend on the state, while mu does)
-- the input features for mu are learned using a linear layer of neurons
+- mu and sigma are both learned (sigma does not depend on the state, while mu does)
+- the input features for mu are learned using a linear layer of neurons given the raw state
 - a vector of parameters is defined for sigma, which are adjusted using the Gaussian Policy Gradient, and are finally 
 summed to yield a scalar, of which the exponential is used as the value for sigma (the standard deviation) 
-- the class of interest is rl.reinforce.agent.TFNeuralNetStochasticPolicyAgent
 - the optimization is done in minibatches, and the batches are shuffled
+- the class of interest is rl.reinforce.agent.TFNeuralNetStochasticPolicyAgent
+- the entry point is the simulator.py script in the rl.reinforce module
 
 - the plot below represents the average total reward for each episode over 10 trials:
 
 <img src="https://raw.githubusercontent.com/lantunes/mountain-car-continuous/master/util/gaussian-policy-gradient-10-runs.png" width="50%"/>
+
+(MountainCarContinuous-v0 defines "solving" as getting an average reward of 90.0 over 100 consecutive trials.)
--- rl/reinforce/simulator.py
+++ rl/reinforce/simulator.py
@@ -2,7 +2,7 @@ import gym
 
 from rl.reinforce.agent import *
 
-# MountainCarContinuous-v0 defines "solving" as getting average reward of 90.0 over 100 consecutive trials.
+# MountainCarContinuous-v0 defines "solving" as getting an average reward of 90.0 over 100 consecutive trials.
 env_name = 'MountainCarContinuous-v0'
 env = gym.make(env_name)
 
@@ -12,41 +12,34 @@ num_actions = env.action_space
 MAX_EPISODES = 200
 MAX_STEPS = 1000
 
-# agent = TFAutoEncodingStochasticPolicyAgent(env, num_input=2, init_learning_rate=1e-5, min_learning_rate=1e-10,
-#                                             learning_rate_N_max=2000, shuffle=True, batch_size=24)
-
-# agent = TFDenoisingAutoEncodingStochasticPolicyAgent(env, num_input=2, init_learning_rate=5e-5, min_learning_rate=1e-10,
-#                                                      learning_rate_N_max=2000, shuffle=True, batch_size=24)
-
-agent = TFNeuralNetStochasticPolicyAgent(env, num_input=2, init_learning_rate=5e-5, min_learning_rate=1e-10,
+agent = TFNeuralNetStochasticPolicyAgent(env, num_input=state_dim, init_learning_rate=5e-5, min_learning_rate=1e-10,
                                          learning_rate_N_max=2000, shuffle=True, batch_size=24, sigma=None)
 
-# agent = TFRecurrentStochasticPolicyAgent(env, num_input=2, init_learning_rate=1e-4, min_learning_rate=1e-10,
-#                                          learning_rate_N_max=2000, shuffle=True, batch_size=1)
+# render an animation of each step in an episode
+render = False
 
-# agent = TFRandomFeaturesStochasticPolicyAgent(env, init_learning_rate=1e-4, min_learning_rate=1e-9,
-#                                               learning_rate_N_max=2000)
+if __name__ == "__main__":
 
-for episode_counter in range(MAX_EPISODES):
-    # initialize
-    state = env.reset()
-    total_rewards = 0
-    sigmas = []
+    for episode_counter in range(MAX_EPISODES):
+        state = env.reset()
+        total_rewards = 0
+        sigmas = []
 
-    done = False
-    for step_counter in range(MAX_STEPS):
-        # env.render()
-        action, sigma = agent.sample_action(state)
-        next_state, reward, done, _ = env.step(action)
+        done = False
+        for step_counter in range(MAX_STEPS):
+            if render:
+                env.render()
+            action, sigma = agent.sample_action(state)
+            next_state, reward, done, _ = env.step(action)
 
-        total_rewards += reward
-        sigmas.append(sigma)
-        agent.store_rollout(state, action, reward)
+            total_rewards += reward
+            sigmas.append(sigma)
+            agent.store_rollout(state, action, reward)
 
-        state = next_state
-        if done:
-            break
+            state = next_state
+            if done:
+                break
 
-    agent.update_model(episode_counter)
+        agent.update_model(episode_counter)
 
-    print("{},{:.2f}".format(episode_counter, total_rewards))
+        print("{},{:.2f}".format(episode_counter, total_rewards))
